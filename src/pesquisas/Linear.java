475f1c45cbd726adfad8eaeeebdca466b2ad112e
Gustavo.Izepon <147446389+Izepongu@users.noreply.github.com>
2024-06-10 20:11:42 -0300

    Update and rename linear.java to Linear.java

--- src/pesquisas/Linear.java
+++ src/pesquisas/Linear.java
@@ -1,6 +1,6 @@
 package ProjetoOrdenaçãoEPesquisa;
 
-public class PesquisaLinear implements MetodoPesquisa {
+public class Linear implements MetodoPesquisa {
     @Override
     public int pesquisar(String[] array, String valor) {
         for (int i = 0; i < array.length; i++) {
