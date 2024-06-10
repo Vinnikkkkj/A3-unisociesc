aa62584af48cd456854f7bef7098935add8e486d
Gustavo.Izepon <147446389+Izepongu@users.noreply.github.com>
2024-06-10 20:03:46 -0300

    Update binaria.java

--- src/pesquisas/binaria.java
+++ src/pesquisas/binaria.java
@@ -1,5 +1,24 @@
-package pesquisas;
+package ProjetoOrdenaçãoEPesquisa;
 
-public class binaria {
+public class PesquisaBinaria implements MetodoPesquisa {
+    @Override
+    public int pesquisar(String[] array, String valor) {
+        int left = 0;
+        int right = array.length - 1;
 
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int comparison = array[mid].compareTo(valor);
+
+            if (comparison == 0) {
+                return mid;
+            }
+            if (comparison < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1; // Valor não encontrado
+    }
 }
