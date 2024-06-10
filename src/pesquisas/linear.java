61f52a3f90e30231d897038142bc8d835a942bdb
Gustavo.Izepon <147446389+Izepongu@users.noreply.github.com>
2024-06-10 20:04:28 -0300

    Update linear.java

--- src/pesquisas/linear.java
+++ src/pesquisas/linear.java
@@ -1,5 +1,13 @@
-package pesquisas;
+package ProjetoOrdenaçãoEPesquisa;
 
-public class linear {
-
+public class PesquisaLinear implements MetodoPesquisa {
+    @Override
+    public int pesquisar(String[] array, String valor) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(valor)) {
+                return i;
+            }
+        }
+        return -1; // Valor não encontrado
+    }
 }
