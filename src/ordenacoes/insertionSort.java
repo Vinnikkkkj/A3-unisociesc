e68c6d9d7ddadbacd2a97f8a6cb864c3b394f717
Gustavo.Izepon <147446389+Izepongu@users.noreply.github.com>
2024-06-10 20:01:42 -0300

    Update insertionSort.java

--- src/ordenacoes/insertionSort.java
+++ src/ordenacoes/insertionSort.java
@@ -1,5 +1,15 @@
-package ordenacoes;
+package ordenacoes
 
-public class insertionSort {
-
+public class InsertionSort {
+    public static void sort(String[] array) {
+        for (int i = 1; i < array.length; i++) {
+            String current = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j].compareTo(current) > 0) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = current;
+        }
+    }
 }
