a3f840a6dfaee431c7cd2e8bdda6323b33b84c8e
Gustavo.Izepon <147446389+Izepongu@users.noreply.github.com>
2024-06-10 20:02:34 -0300

    Update quickSort.java

--- src/ordenacoes/quickSort.java
+++ src/ordenacoes/quickSort.java
@@ -1,5 +1,32 @@
 package ordenacoes;
 
-public class quickSort {
+public class QuickSort {
+    public static void sort(String[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
 
+    private static void quickSort(String[] array, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(array, low, high);
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(String[] array, int low, int high) {
+        String pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j].compareTo(pivot) < 0) {
+                i++;
+                String temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        String temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        return i + 1;
+    }
 }
